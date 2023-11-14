0724c98cd032a7179622b7c9fdede9f13ddd6ab0
David Praschak <david.praschak@univie.ac.at>
2023-11-14 12:22:32 +0100

    Boilerplate fixed.

--- cookingSchool/src/main/java/Cooking/School/Project/cookingSchool/Main.java
+++ cookingSchool/src/main/java/Cooking/School/Project/cookingSchool/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CookingSchoolBackendApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CookingSchoolBackendApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
