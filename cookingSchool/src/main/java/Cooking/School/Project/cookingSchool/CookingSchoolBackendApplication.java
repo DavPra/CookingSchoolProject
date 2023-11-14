72100b25c6dc4881fa0c13968a450e449ef448dc
David Praschak <david.praschak@univie.ac.at>
2023-11-14 11:17:16 +0100

    Boilerplate built

---
+++ cookingSchool/src/main/java/Cooking/School/Project/cookingSchool/CookingSchoolBackendApplication.java
@@ -0,0 +1,13 @@
+package Cooking.School.Project.cookingSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingSchoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingSchoolBackendApplication.class, args);
+	}
+
+}
---
+++ cookingSchool/src/test/java/Cooking/School/Project/cookingSchool/CookingSchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Cooking.School.Project.cookingSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingSchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
