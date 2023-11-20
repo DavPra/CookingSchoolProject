08f767b28d5b75289487cc31e6b2b933016487e2
David Praschak <david.praschak@univie.ac.at>
2023-11-20 15:02:07 +0100

    UserController getUserByID, deleteUserById, updateUser added

--- cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/controller/UserController.java
+++ cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/controller/UserController.java
@@ -3,9 +3,7 @@ package Cooking.School.Project.cookingSchool.controller;
 import Cooking.School.Project.cookingSchool.Services.UserService;
 import Cooking.School.Project.cookingSchool.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -18,6 +16,21 @@ public class UserController {
     @PostMapping("/users")
     public User addUser(@RequestBody User user){
         return  userService.addUser(user);
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUserById(@PathVariable Long id){
+        userService.deleteUserById(id);
+    }
+
+    @PutMapping("/users")
+    public User updateUser(@RequestBody User user){
+        return userService.updateUser(user);
     }
 
 }
