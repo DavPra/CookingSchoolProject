c7a260c6fa6a7a2915778bc46642e6529bce9251
David Praschak <david.praschak@univie.ac.at>
2023-11-20 14:47:33 +0100

    RecipeService Standardoperationen eingefügt

--- cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/Services/RecipeService.java
+++ cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/Services/RecipeService.java
@@ -1,4 +1,35 @@
 package Cooking.School.Project.cookingSchool.Services;
 
+import Cooking.School.Project.cookingSchool.entities.Recipe;
+import Cooking.School.Project.cookingSchool.repository.RecipesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class RecipeService {
+
+    @Autowired
+    RecipesRepository recipesRepository;
+
+    public RecipeService(){
+    }
+
+    public Recipe addRecipe(Recipe recipe){
+        recipesRepository.save(recipe);
+        return recipe;
+    }
+
+    public Recipe getRecipeById(Long id){
+        return recipesRepository.findById(id).get();
+    }
+
+    public void deleteRecipeById(Long id){
+        recipesRepository.deleteById(id);
+    }
+
+    public Recipe updateRecipe(Recipe recipe){
+        recipesRepository.save(recipe);
+        return recipe;
+    }
 }
