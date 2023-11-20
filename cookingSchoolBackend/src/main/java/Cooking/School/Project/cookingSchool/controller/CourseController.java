8053898778f2f9ecdfd4cac0f4b0facd0005b6c7
David Praschak <david.praschak@univie.ac.at>
2023-11-20 15:00:31 +0100

    CourseController addCourse, getCourseByID, deleteCourseById, updateCourse ad

--- cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/controller/CourseController.java
+++ cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/controller/CourseController.java
@@ -1,4 +1,35 @@
 package Cooking.School.Project.cookingSchool.controller;
 
+import Cooking.School.Project.cookingSchool.Services.CourseService;
+import Cooking.School.Project.cookingSchool.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("/courses")
+    public Course addCourse(@RequestBody Course course){
+        return  courseService.addCourse(course);
+    }
+
+    @GetMapping("/courses/{id}")
+    public Course getCourseById(@PathVariable Long id){
+        return courseService.getCourseById(id);
+    }
+
+    @DeleteMapping("/courses/{id}")
+    public void deleteCourseById(@PathVariable Long id){
+        courseService.deleteCourseById(id);
+    }
+
+    @PutMapping("/courses")
+    public Course updateCourse(@RequestBody Course course){
+        return courseService.updateCourse(course);
+    }
+
+
 }
