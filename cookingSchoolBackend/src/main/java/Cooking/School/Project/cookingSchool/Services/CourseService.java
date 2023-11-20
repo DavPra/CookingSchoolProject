c95c25143f55df38edd16cb38914df5e4a131238
David Praschak <david.praschak@univie.ac.at>
2023-11-20 14:47:03 +0100

    CourseService Standardoperationen eingefügt

--- cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/Services/CourseService.java
+++ cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/Services/CourseService.java
@@ -1,4 +1,35 @@
 package Cooking.School.Project.cookingSchool.Services;
 
+import Cooking.School.Project.cookingSchool.entities.Course;
+import Cooking.School.Project.cookingSchool.repository.CoursesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CourseService {
+
+    @Autowired
+    CoursesRepository coursesRepository;
+
+    public CourseService(){
+    }
+
+    public Course addCourse(Course course){
+        coursesRepository.save(course);
+        return course;
+    }
+
+    public Course getCourseById(Long id){
+        return coursesRepository.findById(id).get();
+    }
+
+    public void deleteCourseById(Long id){
+        coursesRepository.deleteById(id);
+    }
+
+    public Course updateCourse(Course course){
+        coursesRepository.save(course);
+        return course;
+    }
+
 }
