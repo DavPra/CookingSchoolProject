d1362862a984f5be07699e7f65a1f456160ecb08
David Praschak <david.praschak@univie.ac.at>
2023-11-22 17:45:33 +0100

    ingredient und tag repositories fixed

--- cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/repository/TagRepository.java
+++ cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/repository/TagRepository.java
@@ -4,5 +4,6 @@ import Cooking.School.Project.cookingSchool.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
