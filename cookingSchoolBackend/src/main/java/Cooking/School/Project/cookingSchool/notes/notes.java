a8e343258f18beaf49fc9db6115a155887ea69e5
David Praschak <david.praschak@univie.ac.at>
2023-12-09 14:52:26 +0100

    notes added

---
+++ cookingSchoolBackend/src/main/java/Cooking/School/Project/cookingSchool/notes/notes.java
@@ -0,0 +1,27 @@
+package Cooking.School.Project.cookingSchool.notes;
+
+
+/*
+
+public class notes {
+
+     for (User user : course.getUsers()) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId(user.getUserId());
+        this.userIds.add(userDTO);
+    }
+}
+
+}
+
+class UserDTO {
+
+    @Getter
+    long userId;
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
+
+ */
