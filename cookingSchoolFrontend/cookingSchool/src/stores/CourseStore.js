f9f4a9b76f2936c31f9767ef21e60a9ab3d447a2
stella <stella.brettschneider@gmx.at>
2023-12-18 19:09:20 +0100

    added CourseStore.js

---
+++ cookingSchoolFrontend/cookingSchool/src/stores/CourseStore.js
@@ -0,0 +1,42 @@
+import {defineStore} from "pinia";
+import axios from "axios";
+
+
+export const useCourseStore = defineStore('course', {
+    state: () => ({
+        courses: []
+    }),
+    actions: {
+        async showCourses() {
+            try {
+                const courseResponse = await axios.get('http://localhost:8082/courses');
+                console.log(courseResponse.data);
+                this.courses = courseResponse.data;
+                console.log('course geladen', courseResponse.data);
+            } catch (error) {
+                console.error('Error loading courses:', error);
+            }
+        },
+        async createCourse(data) {
+            try {
+                const courseData = {
+                    courseTitle: data.courseTitle,
+                    description: data.description,
+                    teacher: data.teacher,
+                    startDate: data.startDate,
+                    maxAttendants: data.maxAttendants,
+                    price: data.price
+                };
+
+                console.log(data);
+
+                const courseResponse = await axios.post('http://localhost:8082/admin/courses', courseData);
+                console.log(courseResponse.data);
+
+                this.courses.push(courseResponse.data);
+            } catch (error) {
+                console.error('Error creating course:', error);
+            }
+        }
+    }
+});
