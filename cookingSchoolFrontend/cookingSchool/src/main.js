3e94eb657da94051a7ce9be4fd7710c8f9c2b501
stella <stella.brettschneider@gmx.at>
2023-12-20 18:39:44 +0100

    fixed vuetify plugin

--- cookingSchoolFrontend/cookingSchool/src/main.js
+++ cookingSchoolFrontend/cookingSchool/src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import {router} from './router'
 import './assets/main.css'
 import {vuetify} from './plugins/vuetify'
-import 'bootstrap/dist/css/bootstrap.css'
 
 const pinia = createPinia()
 
