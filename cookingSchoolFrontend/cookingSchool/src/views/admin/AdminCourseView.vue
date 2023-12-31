8c6908050f827e0611f4b6534dfcf8647d200d76
stella <stella.brettschneider@gmx.at>
2023-12-31 08:57:35 +0100

    daten werden erfolgreich in die db gesaved

--- cookingSchoolFrontend/cookingSchool/src/views/admin/AdminCourseView.vue
+++ cookingSchoolFrontend/cookingSchool/src/views/admin/AdminCourseView.vue
@@ -25,17 +25,8 @@ const data = { //ref entfernt
 
 async function createCourse() {
     console.log('createCourse function called');
-  const formattedDate = new Date(data.startDate).toISOString('en-US', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    timeZone: 'UTC'
-  });
+  const formattedDate = new Date(data.startDate).toISOString();
 
-  // Erstellen der Daten für die Anfrage ans Backend
   const requestData = {
     ...data,
     startDate: formattedDate
