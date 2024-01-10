e7d06b79c68dced00b454077142573a43413ec6a
DavPra <david.prachak@univie.ac.at>
2024-01-10 18:23:31 +0100

    Courselist added

--- cookingSchoolFrontend/cookingSchool/src/views/user/CourseView.vue
+++ cookingSchoolFrontend/cookingSchool/src/views/user/CourseView.vue
@@ -27,42 +27,7 @@ showCourses();
 </script>
 
 <template>
-  <!-- Übersicht aller Kurse zum Bearbeiten für den Admin -->
-
-
-   <div>
-
-    <div class="course-list">
-      <h1>Course List</h1>
-      <v-row>
-        <v-col v-for="course in courseStore.courses" :key="course.courseId" cols="12" sm="6" md="4" lg="3">
-          <v-card>
-            <v-img
-                cover
-                height="250"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-            ></v-img>
-
-            <v-card-title>{{ course.courseTitle }}</v-card-title>
-            <v-card-subtitle>{{ course.startDate }}</v-card-subtitle>
-            <v-card-text>{{ course.description }}</v-card-text>
-            <v-card-actions>
-
-
-              <v-btn @click="editCourse(course)" icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-              <v-btn @click="deleteCourse(course.courseId)" icon>
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </div>
- </div>
-
-
+  <!-- Übersicht aller Kurse für User und Besucher -->
 
 
   <div>
