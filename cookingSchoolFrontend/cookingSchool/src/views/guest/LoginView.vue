3858af606c5ea6155d96e2be9c3fe47e5942e443
Kathy114 <katharina.binder@outlook.com>
2024-01-17 16:15:04 +0100

    Registration button in LoginView ergänzt, noch zu fixen

--- cookingSchoolFrontend/cookingSchool/src/views/guest/LoginView.vue
+++ cookingSchoolFrontend/cookingSchool/src/views/guest/LoginView.vue
@@ -27,21 +27,25 @@
     }
   }
 
-  //TODO: Kathy - Add registration button and link to registration page
+  //TODO: Kathy - Fix registration button and link to registration page
 
 </script>
 
 <template>
   <!-- Formular für einen Guest um sich als User einzuloggen, wenn er sich zuvor registriert hat-->
   <v-card-title class="pt-4">Login</v-card-title>
-  <v-form @submit.prevent="login" style="margin-bottom: 10px;">
+  <v-form @submit.prevent="login">
     <v-card-item>
       <v-alert color="error" v-model="areCredentialsInvalid" closable="true" close-icon="mdi-close" text="Zugangsdaten falsch" class="mb-6"/>
       <v-text-field variant="outlined" required label="Benutzername" v-model="credentials.username" :disabled="isLoginInProgress"/>
       <v-text-field type="password" variant="outlined" required label="Passwort" v-model="credentials.password" :disabled="isLoginInProgress"/>
     </v-card-item>
-    <v-card-actions style="margin: 120px">
+    <v-card-actions style="margin-left: 10px">
       <v-btn color="primary lighten-2" type="submit" variant="elevated" :loading="isLoginInProgress">Login</v-btn>
     </v-card-actions>
+    <v-card-item>
+      <div>Noch keinen Account?</div><br>
+      <v-btn to="/registration">Hier registrieren</v-btn>
+    </v-card-item>
   </v-form>
 </template>
