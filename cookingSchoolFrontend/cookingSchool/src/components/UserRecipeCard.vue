8d67d3d7d8072ad507134076484e60ebdf42c868
David Praschak <david.praschak@univie.ac.at>
2024-01-22 20:48:45 +0100

    Rezepte für user

---
+++ cookingSchoolFrontend/cookingSchool/src/components/UserRecipeCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {useRecipeStore} from "@/stores/RecipeStore";
+import {toRaw} from "vue";
+
+const recipeStore = useRecipeStore();
+//const recipe = defineProps(['title','description', 'difficulty', 'preparation', 'ingredients' ,'recipeId'])
+const { title, description, difficulty, preparation, ingredients, recipeId ,courseIds} = defineProps(['title', 'description', 'difficulty', 'preparation', 'ingredients', 'recipeId','courseIds']);
+const getIngredients = toRaw(ingredients);
+console.log('ingredients:', ingredients);
+
+</script>
+
+<template>
+<v-card>
+
+    <v-img
+            cover
+            height="250"
+            src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        ></v-img>
+
+  <v-card-title>{{title}}</v-card-title>
+  <v-card-subtitle>Difficulty: {{difficulty}} </v-card-subtitle>
+  <v-card-subtitle> Preparation: {{preparation}}</v-card-subtitle>
+  <v-card-text>
+    Zutaten:
+    <ul>
+      <li v-for="(ingredient, index) in ingredients" :key="index">
+        {{ ingredient.quantity }} - {{ ingredient.unit }} - {{ ingredient.title }}
+      </li>
+    </ul>
+  </v-card-text>
+  <v-card-text>Beschreibung: {{description}}</v-card-text>
+</v-card>
+</template>
+
+<style scoped>
+
+</style>
